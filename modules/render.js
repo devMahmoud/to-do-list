d424da5a222d4505b2957584bd1827ddca133342
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-04-28 21:54:30 +0200

    implement add and remove functionality

--- modules/render.js
+++ modules/render.js
@@ -1,21 +1,86 @@
-import todos from './data.js';
+import Todo from './todo.js';
 
 const todosList = document.querySelector('.todos-list');
+const todoInput = document.querySelector('.add-todo');
+const addBtn = document.querySelector('.add');
+const clearBtn = document.querySelector('.clear');
+let todosArr = [];
 
 const render = () => {
   todosList.innerHTML = null;
+  const todo = new Todo();
+  if (localStorage.getItem('todos')) {
+    todosArr = JSON.parse(localStorage.getItem('todos'));
+  }
 
-  for (let i = 0; i < todos.length; i += 1) {
-    const todo = document.createElement('div');
-    todo.className = 'todo';
-    todo.innerHTML = `<div class="todo-check-desc">
-    <input class="todo-check" type="checkbox" name="todo-check">
-    <p class="todo-desc">${todos[i].description}</p>
-    </div>
-    <i class="fa-solid fa-ellipsis-vertical"></i>`;
+  const removeTodo = (index) => {
+    todo.remove(index, todosArr);
+  };
 
-    todosList.append(todo);
+  const updateStatus = (index, bool) => {
+    todosArr[index].completed = bool;
+  };
+
+  for (let i = 0; i < todosArr.length; i += 1) {
+    const todoDiv = document.createElement('div');
+    todoDiv.className = 'todo';
+
+    const checkAndDesc = document.createElement('div');
+    checkAndDesc.className = 'todo-check-desc';
+    const todoCheck = document.createElement('input');
+    todoCheck.type = 'checkbox';
+    todoCheck.className = 'todo-check';
+    const todoDesc = document.createElement('p');
+    todoDesc.textContent = todosArr[i].description;
+    todoDesc.className = 'todo-desc';
+    checkAndDesc.append(todoCheck, todoDesc);
+
+    const todoOptions = document.createElement('i');
+    todoOptions.classList.add('fa-solid', 'fa-ellipsis-vertical', 'todo-options');
+
+    todoDiv.append(checkAndDesc, todoOptions);
+
+    todosList.append(todoDiv);
+
+    todoOptions.addEventListener('click', () => {
+      if (todoOptions.classList.contains('fa-trash-can')) {
+        todoDiv.remove();
+        removeTodo(i);
+      }
+      todoOptions.classList.toggle('fa-ellipsis-vertical');
+      todoOptions.classList.toggle('fa-trash-can');
+    });
+
+    todoCheck.addEventListener('change', () => {
+      if (todoCheck.checked) {
+        console.log('Checkbox is checked..', i);
+        updateStatus(i, true);
+      } else {
+        console.log('Checkbox is not checked..');
+        updateStatus(i, false);
+      }
+    });
   }
 };
+
+const addTodo = () => {
+  if (todoInput.value) {
+    const todo = new Todo(todoInput.value, false, todosArr.length + 1);
+    todo.add(todo, todosArr);
+    render();
+  } else {
+    alert('Task description can not be empty');
+  }
+};
+
+addBtn.addEventListener('click', () => {
+  addTodo();
+});
+
+todoInput.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    addTodo();
+  }
+});
 
 export default render;
---
+++ modules/todo.js
@@ -0,0 +1,19 @@
+class Todo {
+  constructor(description, completed = false, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
+
+  add = (todo, todosArr) => {
+    todosArr.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todosArr));
+  }
+
+  remove = (index, todosArr) => {
+    todosArr.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(todosArr));
+  }
+}
+
+export default Todo;
